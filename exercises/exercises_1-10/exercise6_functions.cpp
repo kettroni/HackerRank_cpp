7383f6616192065b29381a02d8bb0271a053155f
Roni Kettunen <ronikettunen96@gmail.com>
2021-10-18 02:33:38 +0300

    exercise6 solved.

---
+++ exercises/exercises_1-10/exercise6_functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int max_of_four(int a, int b, int c, int d)
+{
+	int biggest = a;
+	if (b > biggest) {
+		biggest = b;
+	}
+	if (c > biggest) {
+		biggest = c;
+	}
+	if (d > biggest) {
+		biggest = d;
+	}
+	return biggest;
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
