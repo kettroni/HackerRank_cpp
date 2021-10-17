9e2f6cabc34f0e27b48a771eeff9df6e426887a4
Roni Kettunen <ronikettunen96@gmail.com>
2021-10-18 02:33:05 +0300

    exercise3 solved.

---
+++ exercises/exercises_1-10/exercise3_basic_data_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+	long b;
+	char ch;
+	float d;
+	double e;
+  
+	scanf("%d %ld %c %f %lf", &a, &b, &ch, &d, &e);
+	printf("%d\n%ld\n%c\n%f\n%lf", a, b, ch, d, e);
+
+	return 0;
+}
